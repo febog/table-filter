9b748eb1ad4002d1f76cde4d786d3adc552f7c81
Felipe Bojorquez <felipe@febog.com>
2026-03-02 16:01:52 -0800

    Pass input directly to function

--- table-filter.js
+++ table-filter.js
@@ -1,14 +1,13 @@
 /**
  * Filters, i.e. hides or shows rows for the given `tableId` table
- * rows according to the `searchInputId` search criteria and updates 
- * the `counterId` element to present the number of displayed rows.
- * @param {string} tableId Table HTML element ID to filter.
- * @param {string} searchInputId Input filed ID containing the search string.
- * @param {string} counterId Element to display the number of visible rows.
+ * according to the given filter terms and updates the `counterId`
+ * element to display the number of visible rows.
+ * @param {string} tableId Id of the `<table>` element whose rows will be filtered.
+ * @param {string} filterString Terms to filter for.
+ * @param {string} counterId Id of an element to display the number of visible rows after filtering.
  */
-function filterTable(tableId, searchInputId, counterId) {
-  const searchInput = document.getElementById(searchInputId);
-  const searchString = searchInput.value.toLowerCase();
+function filterTable(tableId, filterString, counterId) {
+  const searchString = filterString.toLowerCase();
   const searchTerms = searchString.replace(/\s+/g, " ").trim().split(" ");
   const table = document.getElementById(tableId);
   const rows = table.getElementsByTagName("tr");
