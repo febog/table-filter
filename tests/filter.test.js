caf360b249202fb9a3ed6577b0631fb7cdfccf81
Felipe Bojorquez <felipe@febog.com>
2026-04-13 14:55:39 -0700

    Add tests

--- tests/filter.test.js
+++ tests/filter.test.js
@@ -6,3 +6,70 @@ test('my-table has 5 non-heading rows', async ({ page }) => {
   const rows = page.locator('#filter-table tbody tr');
   await expect(rows).toHaveCount(5);
 });
+
+test('Filter "no" rows', async ({ page }) => {
+  await page.goto('/test-stage');
+
+  const filterInput = page.locator('#table-search-input');
+  filterInput.fill('no');
+
+  const hiddenRows = page.locator('#filter-table tbody tr[style="display: none;"]');
+  await expect(hiddenRows).toHaveCount(3);
+});
+
+test('Filter "yes" rows', async ({ page }) => {
+  await page.goto('/test-stage');
+
+  const filterInput = page.locator('#table-search-input');
+  filterInput.fill('yes');
+
+  const hiddenRows = page.locator('#filter-table tbody tr[style="display: none;"]');
+  await expect(hiddenRows).toHaveCount(2);
+});
+
+test('Filter by text', async ({ page }) => {
+  await page.goto('/test-stage');
+
+  const filterInput = page.locator('#table-search-input');
+  filterInput.fill('tempor');
+
+  const hiddenRows = page.locator('#filter-table tbody tr[style="display: none;"]');
+  await expect(hiddenRows).toHaveCount(3);
+});
+
+test('Filter by number', async ({ page }) => {
+  await page.goto('/test-stage');
+
+  const filterInput = page.locator('#table-search-input');
+  filterInput.fill('08');
+
+  const hiddenRows = page.locator('#filter-table tbody tr[style="display: none;"]');
+  await expect(hiddenRows).toHaveCount(2);
+});
+
+test('Filter a single row', async ({ page }) => {
+  await page.goto('/test-stage');
+
+  const filterInput = page.locator('#table-search-input');
+  filterInput.fill('Lorem');
+
+  const hiddenRows = page.locator('#filter-table tbody tr[style="display: none;"]');
+  await expect(hiddenRows).toHaveCount(4);
+});
+
+test('Initial filter', async ({ page }) => {
+  await page.goto('/test-stage');
+
+  const hiddenRows = page.locator('#filter-table tbody tr[style="display: none;"]');
+  await expect(hiddenRows).toHaveCount(0);
+});
+
+test('Small input, with none found', async ({ page }) => {
+  await page.goto('/test-stage');
+
+  const filterInput = page.getByLabel('Search:');
+  filterInput.fill('f');
+
+  const hiddenRows = page.locator('#filter-table tbody tr[style="display: none;"]');
+  await expect(hiddenRows).toHaveCount(5);
+});
